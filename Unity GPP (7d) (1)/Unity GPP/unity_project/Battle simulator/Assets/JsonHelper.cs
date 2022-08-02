88675523c700bc909c703f7e1bce4f0a903c4651
Abb Shamos <bshmos@gmail.com>
2022-08-02 22:31:26 +0300

    Loading function from config file

---
+++ Unity GPP (7d) (1)/Unity GPP/unity_project/Battle simulator/Assets/JsonHelper.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class JsonHelper : MonoBehaviour
+{
+
+    //Usage:
+    //YouObject[] objects = JsonHelper.getJsonArray<YouObject> (jsonString);
+    public static T[] getJsonArray<T>(string json)
+    {
+        string newJson = "{ \"array\": " + json + "}";
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+        return wrapper.array;
+    }
+    //Usage:
+    //string jsonString = JsonHelper.arrayToJson<YouObject>(objects);
+    public static string arrayToJson<T>(T[] array)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.array = array;
+        return JsonUtility.ToJson(wrapper);
+    }
+
+    [Serializable]
+    private class Wrapper<T>
+    {
+        public T[] array;
+    }
+
+    public static T[] FromJson<T>(string json)
+    {
+        Wrappers<T> wrapper = JsonUtility.FromJson<Wrappers<T>>(json);
+        return wrapper.Item;
+    }
+
+    public static string ToJson<T>(T[] array)
+    {
+        Wrappers<T> wrapper = new Wrappers<T>();
+        wrapper.Item = array;
+        return JsonUtility.ToJson(wrapper);
+    }
+
+    public static string ToJson<T>(T[] array, bool prettyPrint)
+    {
+        Wrappers<T> wrapper = new Wrappers<T>();
+        wrapper.Item = array;
+        return JsonUtility.ToJson(wrapper, prettyPrint);
+    }
+
+    [Serializable]
+    private class Wrappers<T>
+    {
+        public T[] Item;
+    }
+}
--- Unity GPP (7d) (1)/Unity GPP/unity_project/Battle simulator/Assets/TeamManager.cs
+++ Unity GPP (7d) (1)/Unity GPP/unity_project/Battle simulator/Assets/TeamManager.cs
@@ -1,19 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
 
 public class TeamManager : MonoBehaviour
 {
+    [Serializable]
+    public class Team
+    {
+        public int teamId;
+        public bool[] teamFormation = new bool[9];
+    }
+
     public int columnLength, rowLength;
     public float x_space, y_space;
     public GameObject Player;
     public GameObject enemy;
     public int availableTroops;
     bool isTeamsAreFormed;
+
+    public Team[] teamtest;
     // Start is called before the first frame update
     void Start()
     {
         isTeamsAreFormed = false;
+
+        if (File.Exists(Application.dataPath + "/config.txt"))
+        {
+            string loadString = File.ReadAllText(Application.dataPath + "/config.txt");
+            Team[] teams = JsonHelper.FromJson<Team>(loadString);
+            Debug.Log(teams[0].teamId);
+        }
     }
 
     // Update is called once per frame
@@ -31,6 +49,8 @@ public class TeamManager : MonoBehaviour
 
     }
 
+
+    //Create a specific team formation for the enemy team
     public void ConstructTeam(GameObject prefab, float startArea, float endArea, int amount)
     {
 
@@ -49,5 +69,13 @@ public class TeamManager : MonoBehaviour
     public void LoadTeamFormation(int id)
     {
 
+    }
+
+    public void Save()
+    {
+        if (File.Exists(Application.dataPath + "/config.txt"))
+        {
+
+        }
     }
 }
